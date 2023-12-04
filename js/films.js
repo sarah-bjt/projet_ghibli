67b0a557bb091e8e2d6e13199dd8711b5ff2fa71
Lison_Kas <lisonfortin@icloud.com>
2023-12-04 23:24:06 +0100

    Partie de l'esthétique engagé etc

--- js/films.js
+++ js/films.js
@@ -11,17 +11,30 @@
 //     table += "</div>"
 //     document.getElementsByClassName('contenant_films').innerHTML = table;
   //document.getElementsByClassName('film') = films;
-
+let i=0;
 fetch('https://ghibliapi.dev/films').then(response => response.json()).then(donnees => {
     let div_films = document.querySelector('#contenant_films');
     donnees.forEach(film => {
-        let new_film = document.createElement('div');
-        new_film.classList.add('film');
+        if(i%2==0){
+            let new_film = document.createElement('div');
+            new_film.classList.add('film');
 
-        new_film.innerHTML += `<img src="${film.image}" alt="image ${film.title}"/>`;  //height="15%" width="15%"   <div id="photo"></div>
-        new_film.innerHTML += `<div class="element_film"><h3>${film.title}</h3><p><b>Director : </b>${film.director}</p><p><b>Released Date : </b>${film.release_date}
-            </p><p>${film.description}</p></div>`;
+            new_film.innerHTML += `<img class="affiches" src="${film.image}" alt="image ${film.title}"/>`;  //height="15%" width="15%"   <div id="photo"></div>
+            new_film.innerHTML += `<div class="element_film"><h3>${film.title}</h3><p><b>Director : </b>${film.director}</p><p><b>Released Date : </b>${film.release_date}
+                </p><p>${film.description}</p></div>`;
 
-        div_films.appendChild(new_film);
+            div_films.appendChild(new_film);
+        }
+        else {
+            let new_film = document.createElement('div');
+            new_film.classList.add('film');
+
+            new_film.innerHTML += `<div class="element_film"><h3>${film.title}</h3><p><b>Director : </b>${film.director}</p><p><b>Released Date : </b>${film.release_date}
+                </p><p>${film.description}</p></div>`;
+            new_film.innerHTML += `<img class="affiches" src="${film.image}" alt="image ${film.title}"/>`;  //height="15%" width="15%"   <div id="photo"></div>
+            
+            div_films.appendChild(new_film);
+        }
+        i++;
     })
 });
