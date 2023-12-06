37690a1b713fa93c05ab284ce41e86dfb5cd9f56
Lison_Kas <lisonfortin@icloud.com>
2023-12-06 17:51:18 +0100

    modifs (que j'ai oublié) et page cosplay

---
+++ js/cosplay.js
@@ -0,0 +1,14 @@
+
+fetch('https://ghibliapi.dev/people').then(response => response.json()).then(donnees => {
+    let div_persos = document.getElementById('contenant_persos');
+    donnees.forEach(persos => {
+            let new_character = document.createElement('div');
+            new_character.classList.add('persos');
+
+            new_character.innerHTML += `<div class="element_persos"><h3>${persos.name}</h3><p><b>Gender : </b>${persos.gender}</p><p><b>Age : </b>${persos.age}
+                </p><p><b>Eye Color : </b>${persos.eye_color}</p><p><b>Hair Color : </b>${persos.hair_color}</p></div>`;
+
+            div_persos.appendChild(new_character);
+
+    })
+});
